47b0316dd131ff8ed7cbc73d3adecc19c1683478
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-21 16:07:17 +0300

    done css+html

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,1 +1,25 @@
 "use strict"
+
+const allTabs = document.querySelectorAll('.tabs__item');
+const allPages = document.querySelectorAll('.pages__item');
+
+
+
+
+for (let i of allTabs) {
+    i.addEventListener('click', function () {
+        for (let k of allTabs) {
+            k.classList.remove('_active')
+        }
+        i.classList.add('_active')
+    })
+}
+
+function openPage() {
+    for (let i of allTabs) {
+        if (i.classList.contains('_active')) {
+            console.log('++', i)
+        }
+    }
+}
+//ДОДЕЛАТЬ ОТКРЫТИЕ НУЖНОЙ СТРАНИЦЫ АУ
